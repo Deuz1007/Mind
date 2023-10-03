0815f4c1d1c48517acaecb771b1657e1dd9b2b56
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-10-03 19:39:58 +0800

    feat: add file size limit to word/pdf

--- app/src/main/java/com/example/mind/utilities/ExtractText.java
+++ app/src/main/java/com/example/mind/utilities/ExtractText.java
@@ -1,9 +1,13 @@
 package com.example.mind.utilities;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.OpenableColumns;
 
+import com.example.mind.exceptions.FileSizeLimitException;
 import com.example.mind.interfaces.PostProcess;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.TextRecognition;
@@ -21,8 +25,22 @@ import java.io.InputStream;
 public class ExtractText {
     private static final TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
     private static final DocumentConverter converter = new DocumentConverter();
+    private static final double FILE_SIZE_LIMIT = 1024 * 1024; //1 MiB
 
-    public static String PDF(Context context, Uri uri) throws IOException {
+    private static void checkFileSize(ContentResolver resolver, Uri uri) throws FileSizeLimitException {
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+
+        int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+        cursor.moveToFirst();
+
+        if (cursor.getDouble(sizeIndex) > FILE_SIZE_LIMIT)
+            throw new FileSizeLimitException();
+    }
+
+    public static String PDF(Context context, Uri uri) throws IOException, FileSizeLimitException {
+        // Check file size
+        checkFileSize(context.getContentResolver(), uri);
+
         // Create an InputStream for the Uri
         InputStream stream = context.getContentResolver().openInputStream(uri);
         assert stream != null;
@@ -46,7 +64,10 @@ public class ExtractText {
         return builder.toString();
     }
 
-    public static String Word(Context context, Uri uri) throws IOException {
+    public static String Word(Context context, Uri uri) throws IOException, FileSizeLimitException {
+        // Check file size
+        checkFileSize(context.getContentResolver(), uri);
+
         // Create an InputStream for the Uri
         InputStream stream = context.getContentResolver().openInputStream(uri);
         assert stream != null;
@@ -54,14 +75,14 @@ public class ExtractText {
         // Extract the raw text
         Result<String> result = converter.extractRawText(stream);
 
-        // Close streams
+        // Close stream
         stream.close();
 
         // Return the extracted text
         return result.getValue();
     }
 
-    public static void Image(Context context, Uri uri, PostProcess callback) throws IOException {
+    public static void Image(Context context, Uri uri, PostProcess callback) throws IOException, FileSizeLimitException {
         recognizer.process(InputImage.fromFilePath(context, uri))
                 .addOnSuccessListener(text -> callback.Success(text.getText()))
                 .addOnFailureListener(callback::Failed);
