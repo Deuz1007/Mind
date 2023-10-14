d7b97f7fd286b18029fdefc892f3a0ea5f55e939
Gian Carlo Dela Cruz <59326022+Deuz1007@users.noreply.github.com>
2023-10-14 22:39:46 +0800

    Change Password

--- app/src/main/java/com/example/mind/UserProfilePage.java
+++ app/src/main/java/com/example/mind/UserProfilePage.java
@@ -2,15 +2,24 @@ package com.example.mind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.mind.models.User;
 
 public class UserProfilePage extends AppCompatActivity {
+
+    Dialog popupDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +31,19 @@ public class UserProfilePage extends AppCompatActivity {
 
         Button btn_logout = findViewById(R.id.signout_btn);
         Button btn_home = findViewById(R.id.go_back_btn);
+        Button btn_change = findViewById(R.id.changepass_btn);
 
         tv_username.setText(User.current.username);
         tv_email.setText(User.current.email);
 
         btn_home.setOnClickListener(view -> startActivity(new Intent(UserProfilePage.this, home_screen.class)));
+
+        btn_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowChangePass();
+            }
+        });
 
         btn_logout.setOnClickListener(view -> {
             // Logout user
@@ -37,5 +54,24 @@ public class UserProfilePage extends AppCompatActivity {
             finish();
         });
 
+    }
+
+    public void ShowChangePass() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(UserProfilePage.this, R.style.AlertDialogTheme);
+        View view = LayoutInflater.from(UserProfilePage.this).inflate(R.layout.activity_change_password, (LinearLayout) findViewById(R.id.changepassview));
+
+        builder.setView(view);
+        EditText newpass = view.findViewById(R.id.newpassword);
+
+        final AlertDialog alertDialog = builder.create();
+
+        view.findViewById(R.id.changepass_button).setOnClickListener(View -> {
+            // Change password Function
+        });
+
+        if (alertDialog.getWindow() != null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
     }
 }
