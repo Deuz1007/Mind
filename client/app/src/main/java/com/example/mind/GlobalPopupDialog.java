f7891a6cc9a6d430360777094f4bd7257765a62d
Gian Carlo Dela Cruz <59326022+Deuz1007@users.noreply.github.com>
2023-10-26 00:27:14 +0800

    feat: create a global notification for user about quiz generation

---
+++ client/app/src/main/java/com/example/mind/GlobalPopupDialog.java
@@ -0,0 +1,14 @@
+package com.example.mind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GlobalPopupDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_global_popup_dialog);
+    }
+}
