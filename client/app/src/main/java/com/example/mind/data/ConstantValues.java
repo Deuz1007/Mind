1f509c59ac983f3f4d149b26a34089abcbada5b0
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-10-25 23:55:07 +0800

    refactor: add next question delay

--- client/app/src/main/java/com/example/mind/data/ConstantValues.java
+++ client/app/src/main/java/com/example/mind/data/ConstantValues.java
@@ -4,4 +4,5 @@ public class ConstantValues {
     public static final long TIMER_TIME = 1000 * 30; // 30 seconds
     public static final long INTERVAL_TIME = 1000; // 1 second
     public static final long BONUS_TIME = 1000 * 5; // 5 seconds
+    public static final long QUESTION_DELAY = 1000 * 2; // 2 seconds
 }
