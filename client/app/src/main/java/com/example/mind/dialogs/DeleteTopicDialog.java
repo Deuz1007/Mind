6a913123796af862082155f4ed0b833c77b03e9b
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-11-15 18:59:28 +0800

    refactor: delete unused class

--- client/app/src/main/java/com/example/mind/dialogs/DeleteTopicDialog.java
+++
@@ -1,9 +0,0 @@
-package com.example.mind.dialogs;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class DeleteTopicDialog extends CustomDialog {
-    public DeleteTopicDialog(AppCompatActivity activity) {
-        super(activity);
-    }
-}
