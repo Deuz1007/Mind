3411181db238457871a53714e162c4e1458b42bc
Gian Carlo Dela Cruz <59326022+Deuz1007@users.noreply.github.com>
2023-11-16 18:41:32 +0800

    Fix: Notification

--- client/app/src/main/java/com/example/mind/data/SocketIO.java
+++ client/app/src/main/java/com/example/mind/data/SocketIO.java
@@ -1,11 +1,17 @@
 package com.example.mind.data;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import com.example.mind.R;
 import com.example.mind.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -19,6 +25,9 @@ public class SocketIO {
     public static Socket instance;
     public static TextView quizNotification;
     public static boolean isNotificationShowing = false;
+    private static final String CHANNEL_ID = "your_channel_id";
+    private static final int NOTIFICATION_ID = 1;
+
 
     public static void createInstance() throws URISyntaxException {
         instance = IO.socket("https://mind-api.onrender.com");
@@ -41,7 +50,42 @@ public class SocketIO {
                     isNotificationShowing = true;
                     quizNotification.setVisibility(View.VISIBLE);
 
-                    User.current = new User(snapshot);
+                    // trigger notif
+//                    NotificationCompat.Builder notificationBuilder =
+//                            new NotificationCompat.Builder(quizNotification.getContext(), "NOTIFICATION_CHANNEL");
+//                    notificationBuilder.setSmallIcon(R.drawable.baseline_lightbulb_circle_24)
+//                            .setContentTitle("QUIZ GENERATAED")
+//                            .setContentText("you may view your quiz")
+//                            .setAutoCancel(true)
+//                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+//
+//                    NotificationManager notificationManager =
+//                            NotificationManager getSystemService(Context.NOTIFICATION_SERVICE);
+//                    notificationManager.notify(0, notificationBuilder.build());
+
+                    // Create a notification manager
+                    NotificationManager notificationManager = (NotificationManager) quizNotification.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+                    // Create a notification channel (required for Android Oreo and above)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        NotificationChannel channel = new NotificationChannel(
+                                CHANNEL_ID,
+                                "Your Channel Name",
+                                NotificationManager.IMPORTANCE_DEFAULT);
+                        notificationManager.createNotificationChannel(channel);
+                    }
+
+                    // Build the notification
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(quizNotification.getContext(), CHANNEL_ID)
+                            .setSmallIcon(android.R.drawable.ic_dialog_info)
+                            .setContentTitle("QUIZ GENERATED")
+                            .setContentText("you may now view your quiz")
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                    // Show the notification
+                    notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+                            User.current = new User(snapshot);
 
                     new Handler().postDelayed(() -> {
                         isNotificationShowing = false;
