a63a6ab07d3e31cc57cc6a9dbcc350a6149c9610
Gian Carlo Dela Cruz <59326022+Deuz1007@users.noreply.github.com>
2023-11-19 23:17:30 +0800

    Fix: Notification tap

--- client/app/src/main/java/com/example/mind/data/SocketIO.java
+++ client/app/src/main/java/com/example/mind/data/SocketIO.java
@@ -6,6 +6,7 @@ import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -112,12 +113,19 @@ public class SocketIO {
             notificationManager.createNotificationChannel(channel);
         }
 
+        // Create an explicit intent for an Activity in your app.
+        Intent intent = new Intent(context, SocketIO.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
         // Build the notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
                 .setSmallIcon(android.R.drawable.ic_dialog_info)
                 .setContentTitle("QUIZ GENERATED")
                 .setContentText("You may view your quiz now")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
 
         Notification notification = builder.build();
 
